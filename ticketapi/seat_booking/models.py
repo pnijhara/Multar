c9bfd122ce9cfeed79e3f3706aeea671e1377458
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2021-02-22 19:27:13 +0530

    Create models
    Signed-off-by: Prajjwal Nijhara <prajjwalnijhara@gmail.com>

--- ticketapi/seat_booking/models.py
+++ ticketapi/seat_booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Seat(models.Model):
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.is_available
+
+class SeatBooking(models.Model):
+    seat_id = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    ticket_id = models.CharField(max_length=64)
+    person_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"${self.seat_id} ${self.ticket_id} ${self.person_name}"
